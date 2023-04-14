4f39a29bfde1b771317d8cfd2484bb9b0259d8e7
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-04-14 18:03:51 +0900

    fix: remove test export

--- packages/in-memory-link/src/index.ts
+++ packages/in-memory-link/src/index.ts
@@ -1,12 +1,4 @@
-import { command, event, query } from '@musubi/core';
-
 export * from './InMemoryReceiverLink';
 export * from './InMemoryClientLink';
 export * from './link';
 export * from './handlers';
-
-export const s = {
-  command,
-  event,
-  query,
-};
