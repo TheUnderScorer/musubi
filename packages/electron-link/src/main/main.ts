2b386f7fde441cca5a0c1be6b082cabb1e67720b
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-04-15 19:14:25 +0900

    chore(electron): fix type problem

--- packages/electron-link/src/main/main.ts
+++ packages/electron-link/src/main/main.ts
@@ -3,14 +3,11 @@ import { IpcMainReceiverLink } from './IpcMainReceiverLink';
 import { IpcMainClientLink } from './IpcMainClientLink';
 import { LinkPair } from '@musubi/core';
 
-export function createMainLink(): LinkPair<
-  IpcMainClientLink,
-  IpcMainReceiverLink
-> {
+export function createMainLink() {
   return {
     receiver: new IpcMainReceiverLink(ipcMain),
     client: new IpcMainClientLink(ipcMain),
-  };
+  } satisfies LinkPair<IpcMainClientLink, IpcMainReceiverLink>;
 }
 
 export { exposeElectronLink } from './expose';
