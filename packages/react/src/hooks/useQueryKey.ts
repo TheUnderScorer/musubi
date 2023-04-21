3f893a7ecb4a8e6bc9344dad78b0d5f6035b9ee9
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-04-21 16:59:34 +0900

    fix(react): add useQueryKey export

--- packages/react/src/hooks/useQueryKey.ts
+++ packages/react/src/hooks/useQueryKey.ts
@@ -1,12 +1,13 @@
 import { useMemo } from 'react';
 
+export function getQueryKey(key: string, variables?: unknown) {
+  return variables ? [key, variables] : [key];
+}
+
 export function useQueryKey(
   query: string,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   variables?: any
 ) {
-  return useMemo(
-    () => (variables ? [query, variables] : [query]),
-    [query, variables]
-  );
+  return useMemo(() => getQueryKey(query, variables), [query, variables]);
 }
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,4 @@
 export * from './musubi';
 export * from './providers/MusubiProvider';
 export * from './hooks.types';
+export * from './hooks/useQueryKey';
