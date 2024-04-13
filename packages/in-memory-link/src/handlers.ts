e3d114309045d0af79094ac7143cfa99d46d3970
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-04-13 20:07:41 +0900

    fix: remove unnecessary handlers

--- packages/in-memory-link/src/handlers.ts
+++ packages/in-memory-link/src/handlers.ts
@@ -1,11 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-  Observable,
-  OperationHandler,
-  OperationName,
-  OperationRequest,
-  OperationResponse,
-} from '@musubi/core';
+import { Observable, OperationRequest, OperationResponse } from '@musubi/core';
 
 export type EventHandler<Ctx = any> = <Payload>(
   request: OperationRequest<Payload, Ctx>
@@ -13,8 +7,6 @@ export type EventHandler<Ctx = any> = <Payload>(
 
 export function createHandlers() {
   return {
-    commandHandlers: new Map<OperationName, OperationHandler<any>>(),
-    queryHandlers: new Map<OperationName, OperationHandler<any>>(),
     operation: new Observable<OperationRequest>(),
     operationResult: new Observable<OperationResponse>(),
     event: new Observable<OperationResponse>(),
